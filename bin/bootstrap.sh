2364933f65847b5458a31dea1431164e523c22b9
Tracey Jaquith <tracey@archive.org>
2022-04-26 17:34:06 -0700

    rare bootstrap timing-hole fix

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,9 +2,11 @@
 
 # try up to ~10m to bootstrap nomad
 
+touch /tmp/bootstrap
 for try in $(seq 0 600)
 do
-  nomad acl bootstrap 2>/dev/null >| /tmp/bootstrap
+  nomad acl bootstrap 2>/tmp/boot.log >> /tmp/bootstrap
   [ "$?" = "0" ] && break
+  ( fgrep 'ACL bootstrap already done' /tmp/boot.log ) && break
   sleep 1
 done
