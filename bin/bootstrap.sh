c0ee23501991454f65ea435a4fe10c81633c05f0
Tracey Jaquith <tracey@archive.org>
2023-01-10 15:08:18 -0800

    better bootstrap setup

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,29 @@
 #!/bin/zsh -u
 
-HIND_FIRST=${HIND_FIRST:-""}
+
+echo      "name = \"$(hostname -s)\"" >> $NOMAD_HCL
+echo "node_name = \"$(hostname -s)\"" >> $CONSUL_HCL
+
+
+if [ $HIND_FIRST ]; then
+  # setup for 2+ VMs to have their nomad and consul daemons be able to talk to each other
+  export FIRSTIP=$(host $HIND_FIRST | perl -ane 'print $F[3] if $F[2] eq "address"' | head -1)
+
+  sed -i -e 's^bootstrap_expect =.*$^^' $CONSUL_HCL
+  echo "encrypt = \"$TOK_C\""        >> $CONSUL_HCL
+  echo "retry_join = [\"$FIRSTIP\"]" >> $CONSUL_HCL
+
+  echo "server { encrypt = \"$TOK_N\" }"               >> $NOMAD_HCL
+  echo "server_join { retry_join = [ \"$FIRSTIP\" ] }" >> $NOMAD_HCL
+  echo "server { bootstrap_expect = 2 }"               >> $NOMAD_HCL
+fi
+
+
+
+# fire up daemons
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+
 
 if [ ! $HIND_FIRST ]; then
 
@@ -21,14 +44,39 @@ if [ ! $HIND_FIRST ]; then
   echo "encrypt = \"$TOK_C\"" >> $CONSUL_HCL
   echo "server { encrypt = \"$TOK_N\" }" >> $NOMAD_HCL
 
+  echo export NOMAD_TOKEN=$(fgrep 'Secret ID' /tmp/bootstrap |cut -f2- -d= |tr -d ' ') > $CONFIG
+  rm -f /tmp/bootstrap
+
+  source $CONFIG
+
 else
 
   # try up to ~5m for consul to be up and happy
   for try in $(seq 0 300)
   do
     sleep 2
-    consul members
+    consul members 2>>/tmp/boot.log >>/tmp/boot.log
     [ "$?" = "0" ] && break
   done
 
+  touch $CONFIG
 fi
+
+
+if [ $HOST_UNAME = Darwin ]; then
+  echo "export NOMAD_ADDR=http://$HOST_HOSTNAME:6000" >> $CONFIG
+else
+  echo "export NOMAD_ADDR=https://$HOST_HOSTNAME"     >> $CONFIG
+fi
+
+
+chmod 400 $CONFIG
+
+
+# verify nomad & consul accessible & working
+echo
+echo
+consul members
+echo
+nomad server members
+echo
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,67 +1,25 @@
 #!/bin/zsh -eu
 
-FI=/etc/hind
+export CONFIG=/etc/hind
 
-HIND_FIRST=${HIND_FIRST:-""}
+export HIND_FIRST=${HIND_FIRST:-""}
 
-if [ ! -e $FI ]; then
-  echo "name = \"$(hostname -s)\"" >> $NOMAD_HCL
-  echo "node_name = \"$(hostname -s)\"" >> $CONSUL_HCL
-
-  if [ $HIND_FIRST ]; then
-    # setup for 2+ VMs to have their nomad and consul daemons be able to talk to each other
-    export FIRSTIP=$(host $HIND_FIRST | perl -ane 'print $F[3] if $F[2] eq "address"' | head -1)
-
-    sed -i -e 's^bootstrap_expect =.*$^^' $CONSUL_HCL
-    echo "encrypt = \"$TOK_C\""        >> $CONSUL_HCL
-    echo "retry_join = [\"$FIRSTIP\"]" >> $CONSUL_HCL
-
-    echo "server { encrypt = \"$TOK_N\" }"               >> $NOMAD_HCL
-    echo "server_join { retry_join = [ \"$FIRSTIP\" ] }" >> $NOMAD_HCL
-    echo "server { bootstrap_expect = 2 }"               >> $NOMAD_HCL
-  fi
-
-
-  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+if [ ! -e $CONFIG ]; then
+  # create a new docker image with the bootstrapped version of your cluster
   ./bin/spinner "Bootstrapping your hind cluster..." /app/bin/bootstrap.sh
 
-
-  if [ ! $HIND_FIRST ]; then
-    echo export NOMAD_TOKEN=$(fgrep 'Secret ID' /tmp/bootstrap |cut -f2- -d= |tr -d ' ') > $FI
-    source $FI
-  else
-    echo 'dont delete this file' > $FI
-  fi
+  ./bin/spinner 'committing bootstrapped image' docker commit hind hind
 
 
+  # now run the new docker image in the background
   typeset -a ARGS
   if [ $HOST_UNAME = Darwin ]; then
     ARGS+=(-p 6000:4646 -p 8000:80 -p 4000:443 -v /sys/fs/cgroup:/sys/fs/cgroup:rw)
-    echo "export NOMAD_ADDR=http://$HOST_HOSTNAME:6000" >> $FI
   else
     ARGS+=(--net=host)
-    if [ ! $HIND_FIRST ]; then
-      echo "export NOMAD_ADDR=https://$(hostname -f)" >> $FI
-    fi
   fi
-
-
-  chmod 400 $FI
-  rm -f /tmp/bootstrap
-
-  # verify nomad & consul accessible & working
-  echo
-  echo
-  consul members
-  echo
-  nomad server members
-  echo
-
-  # create a new docker image with the bootstrapped version of your cluster
-  ./bin/spinner 'committing bootstrapped image' docker commit hind hind
-
-  # now run the new docker image in the background
   docker run $ARGS --privileged -v /var/run/docker.sock:/var/run/docker.sock --restart=always --name hindup -d hind > /dev/null
+
 
   if [ ! $HIND_FIRST ]; then
     echo '
@@ -74,7 +32,7 @@ by setting these environment variables
 (inside or outside the running container or from a home machine --
  anywhere you have downloaded a `nomad` binary):
     '
-    cat $FI
+    cat $CONFIG
   else
     echo '
 
